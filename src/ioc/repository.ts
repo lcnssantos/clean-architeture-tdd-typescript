e721a5fd34c46af0b9f5fbc90b2962b3e63c3da0
Luciano Santos <luciano.ssants@gmail.com>
2020-06-06 15:34:32 -0300

    Implementando singleton para repositório

--- src/ioc/repository.ts
+++ src/ioc/repository.ts
@@ -1,7 +1,22 @@
 import {FileRepository} from "../infra/repositories/files/FileRepository";
 import {Participant} from "../domain/entity/participant";
+import {MemoryRepository} from "../infra/repositories/memory/MemoryRepository";
+import {RepositoryContract} from "../application/repositories/RepositoryContract";
 
+var ParticipantRepository: RepositoryContract<Participant>;
+
+export const ParticipantFileRepositoryFactory = () => {
+    return new FileRepository<Participant>(Participant, 'participants')
+}
+
+export const ParticipantInMemoryRepositoryFactory = () => {
+    return new MemoryRepository<Participant>()
+}
 
 export const ParticipantRepositoryFactory = () => {
-    return new FileRepository<Participant>(Participant, 'participants')
+    if(!ParticipantRepository) {
+        ParticipantRepository = ParticipantFileRepositoryFactory()
+    }
+
+    return ParticipantRepository
 }
